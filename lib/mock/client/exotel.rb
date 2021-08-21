d4aa76514f98730d95b12ca9e2486e23db4c9c77
kiran <kiran.j@trackdfect.com>
2021-08-21 18:08:25 +0530

    changed the method name as get

--- lib/mock/client/exotel.rb
+++ lib/mock/client/exotel.rb
@@ -89,7 +89,7 @@ module Mock
       end
 
       desc 'Dumps responses to the file. Which can be used later to validate using filename'
-      post '/callback_response' do
+      get '/callback_response' do
         data = params
         filename_part = params[:CallSid] + "_01".to_json
         File.open("./callback_json/#{filename_part}.json","a+") {|f| f.puts data.to_json}
